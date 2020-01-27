c9b5d132649f170ed6a0265cbcb496c9d196608c
mdraw <martin.drawitsch@gmail.com>
2020-01-27 20:19:06 +0100

    Support additional options in batch_deface.py

--- batch_deface.py
+++ batch_deface.py
@@ -6,10 +6,13 @@ import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('path', help='Directory with videos to be processed')
+parser.add_argument('--options', default=None, help='Additional options to be passed to deface.py')
 
 args = parser.parse_args()
 
-cmd_base = 'python3 deface.py -m'
+cmd_base = 'python3 deface.py'
+if args.options is not None:
+    cmd_base = f'{cmd_base} {args.options}'
 
 for entry in os.scandir(args.path):
     if entry.is_file():
