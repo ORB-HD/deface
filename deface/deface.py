2f8594404f4541a672bae2127154a567bec3ad1b
mdraw <martin.drawitsch@gmail.com>
2020-02-06 15:57:42 +0100

    Refactor configuration system

--- deface/deface.py
+++ deface/deface.py
@@ -2,8 +2,9 @@
 
 import argparse
 import os
+from typing import Tuple
+
 import tqdm
-import json
 import skimage.draw
 import numpy as np
 import imageio
@@ -13,74 +14,7 @@ import cv2
 from deface.centerface import CenterFace
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-i', default='<video0>', help='Input file name or camera index')
-parser.add_argument('-o', default=None, help='Output file name (defaults to input path + postfix "_anonymized")')
-parser.add_argument('-r', default='blur', choices=['solid', 'blur', 'none'], help='Anonymization filter mode for face regions')
-parser.add_argument('-d', default=None, help='Downsample images for network inference to this size')
-# parser.add_argument('-c', default='red', help='Color hue of the overlays (boxes, texts)')
-parser.add_argument('-l', default=False, action='store_true', help='Enable landmark visualization')
-parser.add_argument('-q', default=False, action='store_true', help='Disable GUI')
-parser.add_argument('-n', default=None, help='Path to CenterFace ONNX model file')
-parser.add_argument('-e', default=False, action='store_true', help='Enable detection enumeration')
-parser.add_argument('-t', default=0.2, type=float, help='Detection threshold')
-parser.add_argument('-m', default=False, action='store_true', help='Use boxes instead of ellipse masks')
-parser.add_argument('-s', default=1.3, type=float, help='Scale factor for face masks (use high values to be on the safe side)')
-parser.add_argument('-b', default='auto', choices=['auto', 'onnxrt', 'opencv'], help='Backend for ONNX model execution')
-parser.add_argument('--nested', default=False, action='store_true', help='Run in nested progress mode (for batch processes)')
-
-args = parser.parse_args()
-
 # TODO: Optionally preserve audio track?
-
-ipath = args.i
-opath = args.o
-replacewith = args.r
-draw_lms = args.l
-show = not args.q
-onnxpath = args.n
-enumerate_dets = args.e
-threshold = args.t
-ellipse = not args.m
-mask_scale = args.s
-backend = args.b
-# ovcolor = colors.get(args.c, (0, 0, 0))
-in_shape = args.d
-if in_shape is not None:
-    w, h = in_shape.split('x')
-    in_shape = int(w), int(h)
-
-
-# cam = isinstance(ipath, int)
-cam = ipath.startswith('<video')
-
-if opath is None and not cam:
-    root, ext = os.path.splitext(ipath)
-    opath = f'{root}_anonymized{ext}'
-
-ovcolor = (0, 0, 255)
-
-centerface = CenterFace(onnxpath, in_shape=in_shape, backend=backend)
-
-
-class Detection:
-    # x1: int
-    # y1: int
-    # x2: int
-    # y2: int
-    bbcoords: np.ndarray
-    score: float
-
-    def __init__(self, x1, y1, x2, y2, score):
-        self.bbcoords = np.round([x1, y1, x2, y2]).astype(int)
-        # self.x1, self.y2, self.x2, self.y2 = self.bbcoords
-        self.score = score
-
-    def scale_bb(self, mask_scale):
-        return scale_bb(*self.bbcoords, mask_scale=mask_scale)
-
-    def scale_bb_(self, mask_scale):
-        self.bbcoords = self.scale_bb(mask_scale=mask_scale)
 
 
 def scale_bb(x1, y1, x2, y2, mask_scale=1.0):
@@ -93,7 +27,13 @@ def scale_bb(x1, y1, x2, y2, mask_scale=1.0):
     return np.round([x1, y1, x2, y2]).astype(int)
 
 
-def draw_det(frame, score, det_idx, x1, y1, x2, y2):
+def draw_det(
+        frame, score, det_idx, x1, y1, x2, y2,
+        replacewith: str = 'blur',
+        ellipse: bool = True,
+        enumerate_dets: bool = False,
+        ovcolor: Tuple[int] = (0, 0, 0)
+):
     if replacewith == 'solid':
         cv2.rectangle(frame, (x1, y1), (x2, y2), ovcolor, -1)
     elif replacewith == 'blur':
@@ -119,7 +59,10 @@ def draw_det(frame, score, det_idx, x1, y1, x2, y2):
         )
 
 
-def anonymize_frame(dets, frame):
+def anonymize_frame(
+        dets, frame, mask_scale,
+        replacewith, ellipse, enumerate_dets, ovcolor
+):
     for i, det in enumerate(dets):
         boxes, score = det[:4], det[4]
         x1, y1, x2, y2 = boxes.astype(int)
@@ -128,15 +71,33 @@ def anonymize_frame(dets, frame):
         y1, y2 = max(0, y1), min(frame.shape[0] - 1, y2)
         x1, x2 = max(0, x1), min(frame.shape[1] - 1, x2)
 
-        draw_det(frame, score, i, x1, y1, x2, y2)
+        draw_det(
+            frame, score, i, x1, y1, x2, y2,
+            replacewith=replacewith,
+            ellipse=ellipse,
+            enumerate_dets=enumerate_dets,
+            ovcolor=ovcolor
+        )
 
 
 def cam_read_iter(reader):
     while True:
         yield reader.get_next_data()
 
-def video_detect():  # Anonymize video using OpenCV
-    # TODO: BGR vs. RGB
+def video_detect(
+        ipath,
+        opath,
+        centerface,
+        threshold,
+        show,
+        cam,
+        nested,
+        replacewith,
+        mask_scale,
+        ellipse,
+        enumerate_dets,
+        ovcolor
+):
     reader: imageio.plugins.ffmpeg.FfmpegFormat.Reader = imageio.get_reader(ipath)
     meta = reader.get_meta_data()
     frame_width, frame_height = meta['size']
@@ -146,7 +107,7 @@ def video_detect():  # Anonymize video using OpenCV
     else:
         read_iter = reader.iter_data()
         nframes = reader.count_frames()
-    if args.nested:
+    if nested:
         bar = tqdm.tqdm(dynamic_ncols=True, total=nframes, position=1, leave=False)
     else:
         bar = tqdm.tqdm(dynamic_ncols=True, total=nframes)
@@ -163,7 +124,10 @@ def video_detect():  # Anonymize video using OpenCV
         # Perform network inference, get bb dets but discard landmark predictions
         dets, _ = centerface(frame, threshold=threshold)
 
-        anonymize_frame(dets, frame)
+        anonymize_frame(
+            dets, frame, mask_scale=mask_scale,
+            replacewith=replacewith, ellipse=ellipse, enumerate_dets=enumerate_dets, ovcolor=ovcolor
+        )
 
         if opath is not None:
             writer.append_data(frame)
@@ -181,7 +145,66 @@ def video_detect():  # Anonymize video using OpenCV
 
 
 def main():
-    video_detect()
+    parser = argparse.ArgumentParser(description='Video anonymization by face detection')
+    parser.add_argument('-i', default='<video0>', help='Input file name or camera index')
+    parser.add_argument('-o', default=None, help='Output file name (defaults to input path + postfix "_anonymized")')
+    parser.add_argument('-r', default='blur', choices=['solid', 'blur', 'none'], help='Anonymization filter mode for face regions')
+    parser.add_argument('-d', default=None, help='Downsample images for network inference to this size')
+    # parser.add_argument('-c', default='red', help='Color hue of the overlays (boxes, texts)')
+    parser.add_argument('-q', default=False, action='store_true', help='Disable GUI')
+    parser.add_argument('-n', default=None, help='Path to CenterFace ONNX model file')
+    parser.add_argument('-e', default=False, action='store_true', help='Enable detection enumeration')
+    parser.add_argument('-t', default=0.2, type=float, help='Detection threshold')
+    parser.add_argument('-m', default=False, action='store_true', help='Use boxes instead of ellipse masks')
+    parser.add_argument('-s', default=1.3, type=float, help='Scale factor for face masks (use high values to be on the safe side)')
+    parser.add_argument('-b', default='auto', choices=['auto', 'onnxrt', 'opencv'], help='Backend for ONNX model execution')
+    parser.add_argument('--nested', default=False, action='store_true', help='Run in nested progress mode (for batch processes)')
+
+    args = parser.parse_args()
+
+    ipath = args.i
+    opath = args.o
+    replacewith = args.r
+    show = not args.q
+    onnxpath = args.n
+    enumerate_dets = args.e
+    threshold = args.t
+    ellipse = not args.m
+    mask_scale = args.s
+    backend = args.b
+    # ovcolor = colors.get(args.c, (0, 0, 0))
+    in_shape = args.d
+    nested = args.nested
+    if in_shape is not None:
+        w, h = in_shape.split('x')
+        in_shape = int(w), int(h)
+
+
+    # cam = isinstance(ipath, int)
+    cam = ipath.startswith('<video')
+
+    if opath is None and not cam:
+        root, ext = os.path.splitext(ipath)
+        opath = f'{root}_anonymized{ext}'
+
+    ovcolor = (0, 0, 0)
+
+    centerface = CenterFace(onnxpath, in_shape=in_shape, backend=backend)
+
+    video_detect(
+        ipath=ipath,
+        opath=opath,
+        centerface=centerface,
+        threshold=threshold,
+        show=show,
+        cam=cam,
+        nested=nested,
+        replacewith=replacewith,
+        mask_scale=mask_scale,
+        ellipse=ellipse,
+        enumerate_dets=enumerate_dets,
+        ovcolor=ovcolor
+    )
 
 
 if __name__ == '__main__':
