b3ed270b9a4140085aa65d96ebb0483ac44e9afa
mdraw <martin.drawitsch@gmail.com>
2020-02-08 18:17:50 +0100

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='deface',
-    version='0.1.0',
+    version='0.2.0',
     author='Martin Drawitsch',
     author_email='martin.drawitsch@gmail.com',
     description='Video anonymization by face detection',
